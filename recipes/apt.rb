9157275d24329c9ac0af92d84911bd035de661b5
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-10-26 17:55:21 +0100

    Makes sure the apt indexes are up2date

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -5,6 +5,8 @@
 # Copyright (C) 2013 Jonas Pfenniger
 #
 
+include_recipe "apt"
+
 cookbook_file "/etc/apt/apt.conf.d/02compress-indexes" do
   owner "root"
   mode 0644
@@ -13,7 +15,6 @@ end
 cookbook_file "/etc/apt/sources.list" do
   owner "root"
   mode 0644
+  notifies :run, "execute[apt-get update]", :immediately
 end
-
-include_recipe "apt"
 
