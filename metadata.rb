e5368f364a6e87fc7eb3344ea8711ca6f507b84e
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-10-28 10:52:51 +0000

    Release v0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jonas@pfenniger.name"
 license          "All rights reserved"
 description      "Installs/Configures base"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "0.3.0"
 
 recipe           "base",           "Installs all the base recipes"
 recipe           "base::ansible",  "Installs ansible on the system"
