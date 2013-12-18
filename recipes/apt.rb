2dd44d582f738fb9ff959b097fb2830722fbd6b1
zimbatm <zimbatm@zimbatm.com>
2013-12-18 12:12:42 +0000

    Introducing cloud-init config
    * Automatically sets the hostname to the instance-id on boot
    * Automatically stripes and mounts ephemeral hard-drives into a single disk
    * Setups the /etc/apt/sources.list with local mirrors

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -11,10 +11,3 @@ cookbook_file "/etc/apt/apt.conf.d/02compress-indexes" do
   owner "root"
   mode 0644
 end
-
-cookbook_file "/etc/apt/sources.list" do
-  owner "root"
-  mode 0644
-  notifies :run, "execute[apt-get update]", :immediately
-end
-
