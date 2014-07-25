002ccbe39b7647cbac002a2441ca1b0c6a207dcd
Aaron McNeal <aaron@mediacore.com>
2014-07-25 09:39:17 -0700

    Add lxc-container to base-image

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -10,4 +10,12 @@
   package pkg
 end
 
+apt_repository "docker" do
+  uri "https://get.docker.io/ubuntu docker"
+  components ["main"]
+  keyserver "hkp://keyserver.ubuntu.com:80"
+  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+end
+
+package "lxc-docker"
 
